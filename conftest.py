075d09a661421d887fccc2101f27df257fbcc56e
alex_mcqueen <mitiacc437@gmail.com>
2025-09-04 16:11:39 +0300

    conftest.py update

--- conftest.py
+++ conftest.py
@@ -1,9 +1,25 @@
 import pytest
+import allure
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+
+    if rep.when == "call" and rep.failed:
+        driver = item.funcargs.get("driver")
+        if driver:
+            allure.attach(
+                driver.get_screenshot_as_png(),
+                name="screenshot_on_failure",
+                attachment_type=allure.attachment_type.PNG,
+            )
 
 
 def pytest_addoption(parser):
@@ -39,12 +55,12 @@ def driver(request):
         driver = webdriver.Firefox(service=service, options=options)
 
     else:
-        raise ValueError(f"❌ Браузер {browser} не поддерживается. Используйте chrome или firefox.")
+        raise ValueError(f"Браузер {browser} не поддерживается. Используйте chrome или firefox.")
 
     if browser_version:
-        print(f"⚙️ Запуск в браузере: {browser}, версия: {browser_version}")
+        print(f"Запуск в браузере: {browser}, версия: {browser_version}")
     else:
-        print(f"⚙️ Запуск в браузере: {browser}, версия: локальная")
+        print(f"Запуск в браузере: {browser}, версия: локальная")
 
     driver.maximize_window()
     yield driver
